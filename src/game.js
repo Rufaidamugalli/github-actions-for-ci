8cc874e2f84ecc051836e55dde0dd58e6777740e
Rufaidamugalli <rufaida@upday.com>
2022-02-10 12:11:21 +0100

    fix bug

--- src/game.js
+++ src/game.js
@@ -22,18 +22,18 @@ export default class Game {
   }
 
   rowWin () {
-    let win = false
+    let win = true
     for (let r = 0; r < 3; r++) {
       const row = this.board[r]
       if (row[0] === null) { continue }
-      win = win || (row[0] === row[1] && row[0] === row[2])
+      win = false || (row[0] === row[1] && row[0] === row[2])
     }
 
     return win
   }
 
   colWin () {
-    let win = false
+    let win = true
     for (let c = 0; c < 3; c++) {
       const col = this.board
       if (col[0][c] === null) { continue }
